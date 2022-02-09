80b627aeee2fcd15d81facbb0e0da997666280f6
Steven Peniche <stevenmpeniche@gmail.com>
2022-02-09 18:50:09 -0500

    Extend HRE with accounts property and deploy task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,24 @@
 require('@nomiclabs/hardhat-waffle');
 
+// Extend HR with accounts property
+extendEnvironment(async (hre) => {
+  hre.accounts = await hre.ethers.getSigners();
+});
+
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
   }
+});
+
+task('deploy', 'Deploys contracts', async (taskArgs, hre) => {
+  const TaskManager = await hre.ethers.getContractFactory('TaskManager');
+  const taskManager = await TaskManager.deploy();
+  await taskManager.deployed();
+
+  console.log('TaskManager deployed to:', taskManager.address);
 });
 
 module.exports = {
