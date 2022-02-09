0ffc6d4f5265768bf9a37db1d0e0f409b56aa829
Steven Peniche <stevenmpeniche@gmail.com>
2022-02-09 10:05:57 -0500

    Add app logic and UI

--- website/app.js
+++ website/app.js
@@ -1,13 +1,144 @@
 App = {
+  account: undefined,
+  taskManager: undefined,
   load: async () => {
-    console.log('Yuuur!');
+    const web3Loaded = await App.loadWeb3();
+    if (web3Loaded) {
+      await App.loadAccount();
+      await App.loadTaskManager();
+      App.configureUI();
+    }
   },
-  // TODO: Load web3 / Connect to Metamask - https://medium.com/valist/how-to-connect-web3-js-to-metamask-in-2020-fee2b2edf58a
-  // TODO: Load account - https://web3js.readthedocs.io/en/v1.7.0/web3-eth.html#getaccounts
-  // TODO: List tasks
-  // TODO: Add task
-  // TODO: Remove task
-  // TODO: Toggle task
+  configureUI: () => {
+    document.querySelector('#add-task').addEventListener('click', App.addTask);
+    document.querySelector('#content').style.display = 'block';
+    document.querySelector('#loading').style.display = 'none';
+  },
+  loadWeb3: async () => {
+    try {
+      if (window.ethereum) {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        window.web3 = new Web3(window.ethereum);
+        return true;
+      }
+      return false;
+    } catch (error) {
+      alert(error.message);
+      return false;
+    }
+  },
+  loadAccount: async () => {
+    try {
+      App.account = (await window.web3.eth.getAccounts())[0];
+      await App.render();
+      // Keep selected account in sync
+      window.ethereum.on('accountsChanged', async (accounts) => {
+        App.account = accounts[0];
+        await App.render(true);
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  loadTaskManager: async () => {
+    try {
+      // Load TaskManager contract schema
+      const response = await fetch('.//TaskManager.json');
+      // Configure TaskManager contract
+      const taskManagerContract = TruffleContract(await response.json());
+      taskManagerContract.setProvider(window.ethereum);
+      // Grab latest TaskManager instance
+      App.taskManager = await taskManagerContract.deployed();
+      await App.render();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  render: async (setLoading) => {
+    try {
+      const contentEl = document.querySelector('#content');
+      const loadingEl = document.querySelector('#loading');
+      const accountEl = document.querySelector('#account');
+      if (setLoading) {
+        contentEl.style.display = 'none';
+        loadingEl.style.display = 'flex';
+      }
+      if (App.taskManager) {
+        accountEl.textContent = App.account;
+        await App.listTasks();
+        if (setLoading) {
+          // Delay UI update
+          setTimeout(() => {
+            loadingEl.style.display = 'none';
+            contentEl.style.display = 'block';
+          }, 500);
+        }
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  listTasks: async () => {
+    try {
+      const taskList = await App.taskManager.listTasks({ from: App.account });
+      let taskListEl = document.querySelector('#task-list');
+      taskListEl.innerHTML = ''; // Clear task list
+      taskList.forEach((task) => {
+        const toggleTaskEl = document.createElement('input');
+        toggleTaskEl.setAttribute('type', 'checkbox');
+        toggleTaskEl.checked = task.complete;
+        toggleTaskEl.classList.add('form-check-input', 'mt-0', 'me-2');
+        toggleTaskEl.addEventListener('click', App.toggleTask);
+        const taskContentEl = document.createElement(task.complete ? 's' : 'span');
+        taskContentEl.innerText = task.content;
+        const removeTaskEl = document.createElement('span');
+        removeTaskEl.innerText = 'delete';
+        removeTaskEl.addEventListener('click', App.removeTask);
+        removeTaskEl.classList.add('link-danger', 'ms-2');
+        const taskListItemEl = document.createElement('li');
+        taskListItemEl.id = task.id;
+        taskListItemEl.classList.add('list-group-item', 'd-flex', 'align-items-center');
+        taskListItemEl.append(toggleTaskEl, taskContentEl, removeTaskEl);
+        taskListEl.append(taskListItemEl);
+      });
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  addTask: async () => {
+    try {
+      let contentInput = document.querySelector('#task-content');
+      const content = contentInput.value.trim();
+      if (content) {
+        await App.taskManager.addTask(content, { from: App.account });
+        contentInput.value = ''; // Clear input
+        await App.render();
+      } else {
+        alert("Task can't be empty");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  removeTask: async (event) => {
+    try {
+      const taskID = event.target.parentElement.id;
+      await App.taskManager.removeTask(taskID, { from: App.account });
+      await App.render();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
+  toggleTask: async (event) => {
+    try {
+      event.preventDefault();
+      const taskID = event.target.parentElement.id;
+      await App.taskManager.toggleTask(taskID, { from: App.account });
+      await App.render();
+    } catch (error) {
+      alert(error.message);
+    }
+  },
 };
 
 window.addEventListener('load', App.load);
